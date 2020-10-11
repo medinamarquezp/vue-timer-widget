9610195fd6241ce37a1e45ebf4879c5d147a4c7a
Pedro Medina <medinamarquezp@gmail.com>
2020-10-11 18:10:33 +0200

    Widget component and App configuration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TimerWidget />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import TimerWidget from "@/components/TimerWidget";
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { TimerWidget }
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/TimerWidget.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="TimerWidget">
+    <TimerConfig />
+    <StartEndHour />
+    <Timer />
+  </div>
+</template>
+
+<script>
+import TimerConfig from "./TimerConfig";
+import StartEndHour from "./StartEndHour";
+import Timer from "./Timer";
+
+export default {
+  name: "TimerWidget",
+  components: { TimerConfig, StartEndHour, Timer }
+};
+</script>
+
+<style lang="scss">
+.TimerWidget {
+  margin: 0 auto;
+  padding: 2rem 3rem;
+  border-radius: 2px;
+  width: 550px;
+  box-sizing: border-box;
+  box-shadow: 2px 2px 20px 0px rgba(0, 0, 0, 0.15);
+}
+</style>
